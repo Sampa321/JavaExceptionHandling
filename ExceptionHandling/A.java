3ad389e9a9bcf0662dcd5e208715a4baa2edc594
Sampa Nayak <sampanayak987@gmail.com>
2025-09-19 23:56:28 +0530

    Add files via upload

--- ExceptionHandling/A.java
+++ ExceptionHandling/A.java
@@ -1,36 +1,15 @@
-package ClassPractice.OOPS.ExceptionHandling;
-
-import java.util.InputMismatchException;
-import java.util.Scanner;
+package PdfNote.ExceptionHandling;
 
 public class A {
-    float a;
-
     public static void main(String[] args) {
-        A obj = new A();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter value for a:");
         try{
-            obj.a = sc.nextFloat();
-
-            //throw new NumberFormatException("Hello,My name is sampa nayak");
-            System.out.println("Division is : "+obj.a/0);
-        }
-        catch (InputMismatchException anyName)
+            throwOne();
+        }catch (IllegalAccessException e)
         {
-            System.out.println(anyName.getMessage());
-            System.out.println("Exception : "+anyName);
+            System.out.println("exception : "+e);
         }
-        catch (NumberFormatException anything)
-        {
-            System.out.println("NumberFormatException msg: "+anything.getMessage());
-        }
-        catch (Exception e)
-        {
-            System.out.println("Exception is : "+e.getMessage());
-            System.out.println("Exception : "+e);
-        }
-        System.out.println("Yeah,i took input from user.So much hardwork!!");
-        System.out.println("Value of a is : "+obj.a);
+    }
+    public static void throwOne() throws IllegalAccessException{
+        throw new IllegalAccessException("demo");
     }
 }
